effd821195506287986a698a3a83a60f7a17b105
Mohdur123 <42186218+Mohdur123@users.noreply.github.com>
2021-05-31 19:03:20 +0300

    Second Commit

---
+++ Tadawul/src/main/java/com/tadawul/TadawulApplication.java
@@ -0,0 +1,13 @@
+package com.tadawul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadawulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadawulApplication.class, args);
+	}
+
+}
